fa27ad4f5c7193cae10683ef5cee048a72009224
corzent <vonehsi@gmail.com>
2022-02-23 17:10:53 +0600

    created models

--- locallibrary/settings.py
+++ locallibrary/settings.py
@@ -108,7 +108,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.0/topics/i18n/
 
-LANGUAGE_CODE = 'ru-ru'
+LANGUAGE_CODE = 'en'
 
 TIME_ZONE = 'Asia/Bishkek'
 
