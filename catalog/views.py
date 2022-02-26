9af242ba1a8f8a2684d381a3ada37849401d3e47
corzent <vonehsi@gmail.com>
2022-02-26 16:02:48 +0600

    basic func is ready

--- catalog/views.py
+++ catalog/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 
 from .models import Book, Author, BookInstance, Genre
 
+from django.views import generic
+
 
 def index(request):
     """
@@ -28,3 +30,21 @@ def index(request):
                  'num_genres': num_genres,
                  'num_books_available': num_books_available},
     )
+
+
+class BookListView(generic.ListView):
+    model = Book
+
+
+class BookDetailView(generic.DetailView):
+    model = Book
+
+
+class AuthorListView(generic.ListView):
+    model = Author
+    paginate_by = 10
+
+
+class AuthorDetailView(generic.DetailView):
+    model = Author
+
